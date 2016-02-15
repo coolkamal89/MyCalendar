f4985ca4194ba8dbf2e1692275f4cd2ba3ac8737
Kamal Relwani <coolkamal89@yahoo.co.in>
2016-02-15 19:30:58 +0530

    Push temporary sql query script.

--- scripts/query.sql
+++ scripts/query.sql
@@ -1,6 +1,12 @@
 CALL spLogout('1', 'CD86F1B6C54981259EC9');
 
+TRUNCATE users;
+
 SELECT * FROM MyDatesDB.users;
 
+SELECT * FROM MyDatesDB.groups;
+
 CALL spLogout('1', 'F9F1A985F4A06EA36974');
+
+CALL spRegister('kamalas', 'abcde12345', 'firstname', 'lastname');
 
