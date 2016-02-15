930dbe08161cfc4c9f66ff813fbbd3b6098e4071
Kamal Relwani <coolkamal89@yahoo.co.in>
2016-02-15 20:00:40 +0530

    Update sql query file.

--- scripts/query.sql
+++ scripts/query.sql
@@ -2,6 +2,8 @@ CALL spLogout('1', 'CD86F1B6C54981259EC9');
 
 TRUNCATE users;
 
+TRUNCATE groups;
+
 SELECT * FROM MyDatesDB.users;
 
 SELECT * FROM MyDatesDB.groups;
@@ -9,4 +11,8 @@ SELECT * FROM MyDatesDB.groups;
 CALL spLogout('1', 'F9F1A985F4A06EA36974');
 
 CALL spRegister('kamalas', 'abcde12345', 'firstname', 'lastname');
+
+CALL spCreateGroup('1', '0250B831CBF55B20A40A', 'Group 122');
+
+CALL spGetGroups('1', '0250B831CBF55B20A40A');
 
